267bf2662f569985e7cc6c396063a2d444a9ff35
johan <johan@LAPTOP-6KO4S5LR>
2025-05-08 19:15:56 +0200

    Add project files.

---
+++ SUT24_Johan_Hansson_Labb4_Enhetstestning/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SUT24_Johan_Hansson_Labb4_Enhetstestning
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            LibrarySystem library = new LibrarySystem();
+            UserInterface.DisplayMenu(library);
+        }
+    }
+}
